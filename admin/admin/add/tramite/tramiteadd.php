30c0464bacabc40ffe941ad0cd86894761ee8926
Ricardo Chavez <rchavesz@ing.ucsc.cl>
2022-12-10 18:58:47 -0300

    Se agrego productos

---
+++ admin/admin/add/tramite/tramiteadd.php
@@ -0,0 +1,10 @@
+<?php
+#Include conn.php file 
+include("../../../key/conn.php");
+#Create a connection to postgresql 
+$conn = pg_connect("host=$host dbname=$database user=$user password=$password");
+#Check if the connection is successful
+if (!$conn) {
+    echo "An error occurred.\n";
+    exit;
+}
