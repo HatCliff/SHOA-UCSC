6a63632644edaa1db7359dac083c894aea61adb5
Hatcliff <hatcliff99@gmail.com>
2022-12-10 16:37:45 -0300

    Añadido el boletín

--- admin/add/producto/boletinadd.php
+++ admin/add/producto/boletinadd.php
@@ -9,5 +9,26 @@ if (!$conn) {
     exit;
 }
 #Get the values from the form boletin.html
+#PRODUCTO
+$rut_empresa = $_POST['rut_empresa'];
+$nombre_empresa = $_POST['nombre'];
+$precio = $_POST['precio'];
+$descripcion = $_POST['descripcion'];
+#BOLETIN
+$mes = $_POST['mes'];
+$anio = $_POST['anio'];
+
+#sql query to insert the values on product
+$sql = "INSERT INTO producto (rut_empresa, nombre, precio, descripcion) 
+        VALUES ('$rut_empresa', '$nombre_empresa', '$precio', '$descripcion')";
+
+
+#sql query to insert the values
+$sql2 = "INSERT INTO boletin (mes, anio) VALUES ('$mes', '$anio')";
+
+#Insert sql query
+$result = pg_query($conn, $sql);
+$result2 = pg_query($conn, $sql2);
+
 
 ?>
